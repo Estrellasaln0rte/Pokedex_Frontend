55ea15303d3542b04fe41cc50a988975186a2f3f
Liz <lizbethherreraortega@gmail.com>
2026-09-07 23:15:01 -0600

    Agregar conexión a endpoint de filtro por tipo

--- src/services/pokeApi.js
+++ src/services/pokeApi.js
@@ -10,3 +10,10 @@ export const getPokemonDetails = async (nombre) => {
   const response = await fetch(`${API_URL}/pokemon/${nombre}`);
   return await response.json();
 };
+
+export const getPokemonesPorTipo = async (tipo) => {
+  const response = await fetch(`${API_URL}/tipos/${tipo}`);
+  if (!response.ok) return [];
+  const data = await response.json();
+  return data.pokemons;
+};
